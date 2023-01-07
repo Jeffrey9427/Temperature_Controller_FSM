3f4842778547d5ebc2f596d928e16be444dc83bc
Jeffrey9427 <jeffguo0927@gmail.com>
2023-01-07 23:32:30 +0700

    update files

---
+++ README.md
@@ -0,0 +1,83 @@
+# Temperature Controller with Finite State Machine
+
+This repository contains the implementation of a temperature controller utilizing the Finite State Machine concept written in C. 
+
+## Description 
+
+This temperature controller with finite state machine is a logical system that controls the temperature of a bath or shower by transitioning between different states based on the input conditions.
+
+It has four states: "Cold", "Increasing", "Hot", and "Decreasing" with "Cold" being the initial state. 
+The input conditions for the FSM are the temperature and the button press. The temperature can be either "Low" (0) or "High" (1), with "Low" representing a temperature below 40°C and "High" representing a temperature of 40°C or higher. The button press can be either "Decrease" (0) or "Increase" (1), with "Decrease" representing the condition when the decrease button is pressed and "Increase" representing the condition when the increase button is pressed. 
+
+The temperature of the shower is controlled by pressing the "Increase" or "Decrease" button. If the temperature is low (below 40°C) and the "Increase" button is pressed, the state will change to "Increasing". If the temperature is high (above 40°C) and the "Increase" button is pressed, the state will change to "Hot".
+
+## FSM Model
+
+#### State Transition Diagram
+<img src="./images/state-transition-diagram.png" width="600">
+
+#### Truth Table 
+
+<img src="./images/truth-table-description.png" width="600">
+
+<img src="./images/truth-table.png" width="600">
+
+```
+Present State = q1q0 -> Cold (00), Increasing (01), Hot (10), Decreasing (11)
+Next State = Q1Q0 -> Cold (00), Increasing (01), Hot (10), Decreasing (11)
+Output = o1o2 -> Cold (00), Increasing (01), Hot (10), Decreasing (11)
+
+Input = i1, i2
+i1 = Temperature [Low (below 40) = 0 / High (at or above 40)= 1]
+i2 = Button [Decrease = 0 / Increase = 1]
+```
+
+#### K-map
+
+- ##### Next State (Q1) K-map
+    <img src="./images/kmap-Q1.png" width="300">
+
+- ##### Next State (Q0) K-map
+    <img src="./images/kmap-Q0.png" width="300">
+
+- ##### Output (o0, o1) K-map
+    <img src="./images/kmap-o0o1.png" width="150">
+
+From all the diagrams and tables, I got the equations for each next state and output: 
+```
+Q1 = (i1 & (!i2)) | (q1 & (!q0)) | (i1 & (!q1)) | ((!i2) & (!q1) & q0)
+Q0 = (i1 & (!i2)) | ((!i1) & (!q1) & q0) | ((!i1) & i2 & (!q1)) | (i1 & q1 & q0)
+
+o0 = q1
+o1 = q0
+```
+
+## How to Run
+
+Follow these steps to run and test the code: 
+
+1. Download the zip folder of all the files in this repository 
+2. Open the unziped folder in your IDE (VSCode).
+3. In your IDE, type command `make` in your IDE terminal and the program will start running.
+4. If you do not use an IDE, open your terminal and direct to this corresponding folder using `cd <folder path>` and type command `make`
+
+
+## Testing
+
+<img src="./images/output1.png" width="500">
+
+This is the first view of the program where it starts from the initial state (00 = "COLD") and it asks for inputs such as the temperature and the button. 
+
+<img src="./images/output2.png" width="500">
+
+After entering the temperature and button, it will print out the next state, the output, and the temperature controller with green colour in which button is pressed. 
+
+The program will rerun and print the new temperature controller according to the next state that we got from the last inputs. In this case, the temperature will still in cold state because the temperature is already at its minimum level (i.e., below 40°C) and the button press is "Decrease" (0), so the temperature cannot decrease further (the previous state is "Cold" (00) and the temperature is "Low" (0) and the button press is "Decrease" (0))
+
+#### Further Testing
+
+<img src="./images/output3.png" width="500">
+
+<img src="./images/output4.png" width="500">
+
+<img src="./images/output5.png" width="500">
--- main.c
+++ main.c
@@ -62,6 +62,43 @@ void cold() {
     printf("            ---------------------------------\n\n");
 }
 
+void cold_increase() {
+    printf("            ----------------------------------\n");
+    printf("           /                                  \\\n");
+    printf("          |     --------------------------     |\n");
+    printf("          |    |");
+    white_on_blue();
+    printf("                          ");
+    reset();
+    printf("|    |\n");
+    printf("          |    |");
+    white_on_blue();
+    printf("   The temperature is     ");
+    reset();
+    printf("|    |\n");
+    printf("          |    |");
+    white_on_blue();
+    printf("           \033[1;37mCOLD           ");
+    reset();
+    printf("|    |\n");
+    printf("          |    |");
+    white_on_blue();
+    printf("                          ");
+    reset();
+    printf("|    |\n");
+    printf("          |     --------------------------     |\n");
+    printf("          |                                    |\n");
+    printf("          |     ---------        ---------     |\n");
+    printf("          |    |    -    |      |");
+    white_on_green();
+    printf("    +    ");
+    reset();
+    printf("|    |\n");
+    printf("          |     ---------        ---------     |\n");
+    printf("           \\                                  /\n");
+    printf("            ---------------------------------\n\n");
+}
+
 void cold_decrease() {
     printf("            ----------------------------------\n");
     printf("           /                                  \\\n");
@@ -126,6 +163,290 @@ void increasing() {
     printf("          |     --------------------------     |\n");
     printf("          |                                    |\n");
     printf("          |     ---------        ---------     |\n");
+    printf("          |    |    -    |      |    +    |    |\n");
+    printf("          |     ---------        ---------     |\n");
+    printf("           \\                                  /\n");
+    printf("            ---------------------------------\n\n");
+}
+
+void increasing_increase() {
+    printf("            ----------------------------------\n");
+    printf("           /                                  \\\n");
+    printf("          |     --------------------------     |\n");
+    printf("          |    |");
+    white_on_red();
+    printf("                          ");
+    reset();
+    printf("|    |\n");
+    printf("          |    |");
+    white_on_red();
+    printf("   The temperature is     ");
+    reset();
+    printf("|    |\n");
+    printf("          |    |");
+    white_on_red();
+    printf("       \033[1;37mINCREASING         ");
+    reset();
+    printf("|    |\n");
+    printf("          |    |");
+    white_on_red();
+    printf("                          ");
+    reset();
+    printf("|    |\n");
+    printf("          |     --------------------------     |\n");
+    printf("          |                                    |\n");
+    printf("          |     ---------        ---------     |\n");
+    printf("          |    |    -    |      |");
+    white_on_green();
+    printf("    +    ");
+    reset();
+    printf("|    |\n");
+    printf("          |     ---------        ---------     |\n");
+    printf("           \\                                  /\n");
+    printf("            ---------------------------------\n\n");
+}
+
+void increasing_decrease() {
+    printf("            ----------------------------------\n");
+    printf("           /                                  \\\n");
+    printf("          |     --------------------------     |\n");
+    printf("          |    |");
+    white_on_red();
+    printf("                          ");
+    reset();
+    printf("|    |\n");
+    printf("          |    |");
+    white_on_red();
+    printf("   The temperature is     ");
+    reset();
+    printf("|    |\n");
+    printf("          |    |");
+    white_on_red();
+    printf("       \033[1;37mINCREASING         ");
+    reset();
+    printf("|    |\n");
+    printf("          |    |");
+    white_on_red();
+    printf("                          ");
+    reset();
+    printf("|    |\n");
+    printf("          |     --------------------------     |\n");
+    printf("          |                                    |\n");
+    printf("          |     ---------        ---------     |\n");
+    printf("          |    |");
+    white_on_green();
+    printf("    -    ");
+    reset();
+    printf("|      |    +    |    |\n");
+    printf("          |     ---------        ---------     |\n");
+    printf("           \\                                  /\n");
+    printf("            ---------------------------------\n\n");
+}
+
+void hot() {
+    printf("            ----------------------------------\n");
+    printf("           /                                  \\\n");
+    printf("          |     --------------------------     |\n");
+    printf("          |    |");
+    white_on_red();
+    printf("                          ");
+    reset();
+    printf("|    |\n");
+    printf("          |    |");
+    white_on_red();
+    printf("   The temperature is     ");
+    reset();
+    printf("|    |\n");
+    printf("          |    |");
+    white_on_red();
+    printf("           \033[1;37mHOT            ");
+    reset();
+    printf("|    |\n");
+    printf("          |    |");
+    white_on_red();
+    printf("                          ");
+    reset();
+    printf("|    |\n");
+    printf("          |     --------------------------     |\n");
+    printf("          |                                    |\n");
+    printf("          |     ---------        ---------     |\n");
+    printf("          |    |    -    |      |    +    |    |\n");
+    printf("          |     ---------        ---------     |\n");
+    printf("           \\                                  /\n");
+    printf("            ---------------------------------\n\n");
+}
+
+void hot_decrease() {
+    printf("            ----------------------------------\n");
+    printf("           /                                  \\\n");
+    printf("          |     --------------------------     |\n");
+    printf("          |    |");
+    white_on_red();
+    printf("                          ");
+    reset();
+    printf("|    |\n");
+    printf("          |    |");
+    white_on_red();
+    printf("   The temperature is     ");
+    reset();
+    printf("|    |\n");
+    printf("          |    |");
+    white_on_red();
+    printf("           \033[1;37mHOT           ");
+    reset();
+    printf("|    |\n");
+    printf("          |    |");
+    white_on_red();
+    printf("                          ");
+    reset();
+    printf("|    |\n");
+    printf("          |     --------------------------     |\n");
+    printf("          |                                    |\n");
+    printf("          |     ---------        ---------     |\n");
+    printf("          |    |");
+    white_on_green();
+    printf("    -    ");
+    reset();
+    printf("|      |    +    |    |\n");
+    printf("          |     ---------        ---------     |\n");
+    printf("           \\                                  /\n");
+    printf("            ---------------------------------\n\n");
+}
+
+void hot_increase() {
+    printf("            ----------------------------------\n");
+    printf("           /                                  \\\n");
+    printf("          |     --------------------------     |\n");
+    printf("          |    |");
+    white_on_red();
+    printf("                          ");
+    reset();
+    printf("|    |\n");
+    printf("          |    |");
+    white_on_red();
+    printf("   The temperature is     ");
+    reset();
+    printf("|    |\n");
+    printf("          |    |");
+    white_on_red();
+    printf("           \033[1;37mHOT            ");
+    reset();
+    printf("|    |\n");
+    printf("          |    |");
+    white_on_red();
+    printf("                          ");
+    reset();
+    printf("|    |\n");
+    printf("          |     --------------------------     |\n");
+    printf("          |                                    |\n");
+    printf("          |     ---------        ---------     |\n");
+    printf("          |    |    -    |      |");
+    white_on_green();
+    printf("    +    ");
+    reset();
+    printf("|    |\n");
+    printf("          |     ---------        ---------     |\n");
+    printf("           \\                                  /\n");
+    printf("            ---------------------------------\n\n");
+}
+
+void decreasing() {
+    printf("            ----------------------------------\n");
+    printf("           /                                  \\\n");
+    printf("          |     --------------------------     |\n");
+    printf("          |    |");
+    white_on_blue();
+    printf("                          ");
+    reset();
+    printf("|    |\n");
+    printf("          |    |");
+    white_on_blue();
+    printf("   The temperature is     ");
+    reset();
+    printf("|    |\n");
+    printf("          |    |");
+    white_on_blue();
+    printf("       \033[1;37mDECREASING         ");
+    reset();
+    printf("|    |\n");
+    printf("          |    |");
+    white_on_blue();
+    printf("                          ");
+    reset();
+    printf("|    |\n");
+    printf("          |     --------------------------     |\n");
+    printf("          |                                    |\n");
+    printf("          |     ---------        ---------     |\n");
+    printf("          |    |    -    |      |    +    |    |\n");
+    printf("          |     ---------        ---------     |\n");
+    printf("           \\                                  /\n");
+    printf("            ---------------------------------\n\n");
+}
+
+void decreasing_decrease() {
+    printf("            ----------------------------------\n");
+    printf("           /                                  \\\n");
+    printf("          |     --------------------------     |\n");
+    printf("          |    |");
+    white_on_blue();
+    printf("                          ");
+    reset();
+    printf("|    |\n");
+    printf("          |    |");
+    white_on_blue();
+    printf("   The temperature is     ");
+    reset();
+    printf("|    |\n");
+    printf("          |    |");
+    white_on_blue();
+    printf("       \033[1;37mDECREASING         ");
+    reset();
+    printf("|    |\n");
+    printf("          |    |");
+    white_on_blue();
+    printf("                          ");
+    reset();
+    printf("|    |\n");
+    printf("          |     --------------------------     |\n");
+    printf("          |                                    |\n");
+    printf("          |     ---------        ---------     |\n");
+    printf("          |    |");
+    white_on_green();
+    printf("    -    ");
+    reset();
+    printf("|      |    +    |    |\n");
+    printf("          |     ---------        ---------     |\n");
+    printf("           \\                                  /\n");
+    printf("            ---------------------------------\n\n");
+}
+
+void decreasing_increase() {
+    printf("            ----------------------------------\n");
+    printf("           /                                  \\\n");
+    printf("          |     --------------------------     |\n");
+    printf("          |    |");
+    white_on_blue();
+    printf("                          ");
+    reset();
+    printf("|    |\n");
+    printf("          |    |");
+    white_on_blue();
+    printf("   The temperature is     ");
+    reset();
+    printf("|    |\n");
+    printf("          |    |");
+    white_on_blue();
+    printf("       \033[1;37mDECREASING         ");
+    reset();
+    printf("|    |\n");
+    printf("          |    |");
+    white_on_blue();
+    printf("                          ");
+    reset();
+    printf("|    |\n");
+    printf("          |     --------------------------     |\n");
+    printf("          |                                    |\n");
+    printf("          |     ---------        ---------     |\n");
     printf("          |    |    -    |      |");
     white_on_green();
     printf("    +    ");
@@ -137,52 +458,33 @@ void increasing() {
 }
 
 
-
 int main(void){
     int q1=0, q0=0, i1=0, i2=0, Q1=0, Q0=0, o0 = 0, o1 = 0;
     while(true){
-        
-        if ((Q1 == 0) && (Q0 == 0) && (o0 == 0 && o1 == 0)) { 
-            printf("Current state: %d %d (Cold)\n\n",Q1,Q0);
+        if ((o0 == 0) && (o1 == 0)) {
+            printf("\n\n\n\n--------------------------------------------------------------");
+            printf("\n\nCurrent state: %d %d (Cold)\n\n", o0, o1);
             cold();
-        } 
-        else if ((Q1 == 0) && (Q0 == 0) && (o0 == 1 && o1 == 0)) {
-            if (i2 == 0) {
-                printf("Current state: %d %d (Cold)\n\n",Q1,Q0);
-                cold_decrease();
-            }
         }
-        else if ((Q1 == 0) && (Q0 == 1)) {
-            if (i2 == 1) {
-                printf("Current state: %d %d (Increasing)\n\n",Q1,Q0);
-                increasing();
-            }
+        else if ((o0 == 0) && (o1 == 1)) {
+            printf("\n\n\n\n--------------------------------------------------------------");
+            printf("\n\nCurrent state: %d %d (Increasing)\n\n", o0, o1);
+            increasing();
         }
-        else if ((Q1 == 1) && (Q0 == 0)) {
-            if (i2 == 0) {
-                printf("Current state: %d %d (Increasing)\n\n",Q1,Q0);
-                // hot_decease();
-            }
-            else if (i2 == 1){
-                printf("Current state: %d %d (Hot)\n\n",Q1,Q0);
-                // hot_increase();
-            }
+        else if ((o0 == 1) && (o1 == 0)) {
+            printf("\n\n\n\n--------------------------------------------------------------");
+            printf("\n\nCurrent state: %d %d (Hot)\n\n", o0, o1);
+            hot();
         }
-        else if ((Q1 == 0) && (Q0 == 1) && (o0 = 0 && o1 == 1)) {
-            if (i2 == 0) {
-                // decreasing_decrease();
-                printf("Current state: %d %d (Decreasing)\n\n",Q1,Q0);
-            }
-            else if (i2 == 1){
-                // decreasing_increase();
-                printf("Current state: %d %d (Decreasing)\n\n",Q1,Q0);
-            }
+        else if ((o0 == 1) && (o1 == 1)) {
+            printf("\n\n\n\n--------------------------------------------------------------");
+            printf("\n\nCurrent state: %d %d (Decreasing)\n\n", o0, o1);
+            decreasing();
         }
-        
 
         i1 = 0;
         i2 = 0;
-
+    
         printf("Temperature [0 for Low (<40) // 1 for High (>40)]: ");
         scanf("%d", &i1);
         printf("Button [0 for Decrease Temperature // 1 for Increase Temperature]: ");
@@ -193,13 +495,50 @@ int main(void){
             printf("\nNext state: %d %d\n", Q1, Q0);
 
             output(q1, q0, &o0, &o1);
-            printf("Output: %d %d\n\n\n\n\n\n\n", o0, o1);
+            printf("Output: %d %d\n", o0, o1);
+            printf("\n");
 
-            q1 = Q1;
-            q0 = Q0;
         } else {
             printf("Sorry, your input is invalid, try again.\n\n");
         }
+
+
+        if ((o0 == 0) && (o1 == 0)) {
+            if (i2 == 0) {
+                cold_decrease();
+            }
+            else {
+                cold_increase();
+            }
+        } 
+        else if ((o0 == 0) && (o1 == 1)) {
+            if (i2 == 0) {
+                increasing_decrease();
+            }
+            else {
+                increasing_increase();
+            }
+        } 
+        else if ((o0 == 1) && (o1 == 0)) {
+            if (i2 == 0) {
+                hot_decrease();
+            }
+            else {
+                hot_increase();
+            }
+        } 
+        else if ((o0 == 1) && (o1 == 1)) {
+            if (i2 == 0) {
+                decreasing_decrease();
+            }
+            else {
+                decreasing_increase();
+            }
+        } 
+
+        q1 = Q1;
+        q0 = Q0;
+        output(q1, q0, &o0, &o1);
 
     }
 
